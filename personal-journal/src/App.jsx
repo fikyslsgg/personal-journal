56c08c0ea85571180c631babb4f497fea7b7fd88
fikyslsgg <yurafikys@gmail.com>
2023-12-14 22:38:52 +0300

    feat: add JournalItem.

--- personal-journal/src/App.jsx
+++ personal-journal/src/App.jsx
@@ -1,10 +1,28 @@
 import './App.css';
 import Button from './components/Button/Button';
+import JournalItem from './components/JournalItem/JournalItem';
 
 function App() {
+  const data = [
+    {
+      title: 'Подготовка к обновлению курсов',
+      date: new Date(),
+      text: 'Сегодня провёл весь день за...'
+    },
+    {
+      title: 'Поход в годы',
+      date: new Date(),
+      text: 'Думал, что очень много време...'
+    }
+  ];
   return (
     <>
       <Button />
+      <JournalItem
+        title={data[0].title}
+        date={data[0].date}
+        text={data[0].text}
+      />
     </>
   );
 }
