5e8f9ac1e8b40eecfbb794833e48edc24c06308a
fikyslsgg <yurafikys@gmail.com>
2023-12-19 19:11:56 +0300

    feat: layout.

--- personal-journal/src/App.jsx
+++ personal-journal/src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css';
-import Button from './components/Button/Button';
 import JournalItem from './components/JournalItem/JournalItem';
 import CardButton from './components/CardButton/CardButton';
+import LeftPanel from './layouts/LeftPanel/LeftPanel';
+import Body from './layouts/Body/Body';
+import Header from './components/Header/Header';
+import JournalList from './components/JournalList/JournalList';
+import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 
 function App() {
   const data = [
@@ -17,23 +21,29 @@ function App() {
     }
   ];
   return (
-    <>
-      <Button />
-      <CardButton>
-        <JournalItem
-          title={data[0].title}
-          date={data[0].date}
-          text={data[0].text}
-        />
-      </CardButton>
-      <CardButton>
-        <JournalItem
-          title={data[1].title}
-          date={data[1].date}
-          text={data[1].text}
-        />
-      </CardButton>
-    </>
+    <div className="app">
+      <LeftPanel>
+        <Header />
+        <JournalAddButton />
+        <JournalList>
+          <CardButton>
+            <JournalItem
+              title={data[0].title}
+              date={data[0].date}
+              text={data[0].text}
+            />
+          </CardButton>
+          <CardButton>
+            <JournalItem
+              title={data[1].title}
+              date={data[1].date}
+              text={data[1].text}
+            />
+          </CardButton>
+        </JournalList>
+      </LeftPanel>
+      <Body>Body</Body>
+    </div>
   );
 }
 
