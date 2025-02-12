9c74f855d2c7c3d25ad0db812a895b752cab2e8c
Yury S <yurafikys@gmail.com>
2025-02-12 17:55:43 +0300

    fix: Fix base url

--- api/api.js
+++ api/api.js
@@ -1,2 +1,2 @@
 export const BASE_URL =
-	'https://raw.githubusercontent.com/fikyslsgg/personal-journal/6a6c19865a4be3860562a7e519d6591bb9a1a739/public';
+	'https://raw.githubusercontent.com/yurysidyakin/personal-journal/6a6c19865a4be3860562a7e519d6591bb9a1a739/public';
